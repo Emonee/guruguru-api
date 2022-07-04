68e4bda71c3f7a1b87baee26b26a2e886310ba2f
Emiliano <e.acevedo.felip@gmail.com>
2022-07-04 11:55:15 -0400

    Cors config

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import cors from "cors"
+
 import guruTranslate from './helpers/translator.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(cors({ methods: 'POST' }))
 app.use(express.static('public'))
 app.use(express.json())
 
