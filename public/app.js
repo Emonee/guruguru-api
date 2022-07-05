a1e41b62f0c8f8d18cce078dd043bc90fc4a1a6e
Emiliano <e.acevedo.felip@gmail.com>
2022-07-04 22:26:51 -0400

    copy function corrections

--- public/app.js
+++ public/app.js
@@ -1,34 +1,6 @@
 const $ = querySelector => document.querySelector(querySelector)
 const $$ = querySelector => document.querySelectorAll(querySelector)
 
-const displayElement = element => {
-  element.classList.remove('hidden')
-  element.classList.add('block')
-}
-
-const hideElement = element => {
-  element.classList.remove('block')
-  element.classList.add('hidden')
-}
-
-const getGuruTranslation = async text => {
-  const request = fetch(`/api/short/${text}`)
-  const response = await request
-  if (!response.ok) {
-    translationParagraph.innerHTML = 'quedo la cagá'
-    return
-  }
-  const textData = await response.text()
-  return textData
-}
-
-const copyToClipboard = text => {
-  navigator.clipboard.writeText(text).then(
-    () => alert('Text Copied!'), 
-    () => alert('Error on copy')
-  )
-}
-
 const form = $('#guru-form')
 const translationParagraph = $('#translation')
 const codeShortString = $('#short-string-example')
@@ -36,6 +8,25 @@ const codeLongString = $('#long-string-example')
 const copyCodeButton = $('#copy-code')
 const copyTranslationButtonn = $('#copy-translation')
 const spansText = $$('.span-text')
+
+const displayElement = element => element.classList.remove('hidden')
+
+const hideElement = element => element.classList.add('hidden')
+
+const getGuruTranslation = async text => {
+  const request = fetch(`/api/short/${text}`)
+  const response = await request
+  if (!response.ok) return 'Error on the fetch, please try again.'
+  const textData = await response.text()
+  return textData
+}
+
+const copyToClipboard = text => {
+  navigator.clipboard.writeText(text).then(
+    () => alert('Text Copied!'),
+    () => alert('Error on copy')
+  )
+}
 
 form.addEventListener('submit', async event => {
   event.preventDefault()
@@ -46,19 +37,19 @@ form.addEventListener('submit', async event => {
   } else {
     displayElement(codeLongString)
     hideElement(codeShortString)
-  }  
+  }
   const data = Object.fromEntries(new FormData(event.target))
   const { text } = data
-  spansText.forEach(span => { span.innerText = text })
+  spansText.forEach(span => { span.textContent = text })
   translationParagraph.textContent = await getGuruTranslation(text)
 })
 
-copyCodeButton.addEventListener('click', () => {
-  const text = codeShortString.innerText
+copyTranslationButtonn.addEventListener('click', () => {
+  const text = translationParagraph.textContent
   copyToClipboard(text)
 })
 
-copyTranslationButtonn.addEventListener('click', () => {
-  const text = translationParagraph.innerText
+copyCodeButton.addEventListener('click', () => {
+  const text = codeShortString.classList.contains('block') ? codeShortString.innerText : codeLongString.innerText
   copyToClipboard(text)
 })
